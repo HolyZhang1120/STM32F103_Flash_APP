995860fdb44fad53dbac1510bf139bfdc9730858
HolyZhang1120 <15803028040@163.com>
2020-10-22 10:15:27 +0800

    STM32F103-IAP升级APPPP

---
+++ ATK-ESP8266/Upgrade.h
@@ -0,0 +1,18 @@
+#ifndef __UPGRADE_H
+#define __UPGRADE_H
+
+#include "sys.h"
+
+#define BATA_SIZE    5120		//下行包有效数据大小
+#define Flash_ADDR   0x1000		//存入flash起始位置
+
+extern u32 Crc_file;
+u8 connect_server(void);
+u8 send_server(u8 *buffer);
+u32 Get_Upgrade_size(void);
+void *Get_Upgrade_package(u8 package,u16 size_num);
+void Upgrade(void);
+void SoftReset(void);
+#endif
+
+
